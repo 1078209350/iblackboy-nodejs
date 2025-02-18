dea5469e6c14fe8568144776f035022b3ac9fc6b
关童 <1078209350@qq.com>
2025-02-18 09:51:07 +0800

    区分环境配置，1.开发环境 2.生产环境

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
 
-dotenv.config();
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 const app = express();
 
 // 中间件
@@ -14,7 +14,7 @@ app.use(express.json()); // 解析 JSON 请求体
 app.use('/user', userRoutes);
 
 // 启动服务器
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
